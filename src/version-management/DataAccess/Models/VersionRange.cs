080351e7c47ba8b979b3daacd4ec4310b6e7c5d2
tintoy <tintoy@tintoy.io>
2016-02-02 20:19:44 +1100

    Tidy up indent. EF7 sucks, so I'm putting this project on hold until they get their shit together and implement complex types.

--- src/version-management/DataAccess/Models/VersionRange.cs
+++ src/version-management/DataAccess/Models/VersionRange.cs
@@ -9,32 +9,32 @@ namespace DD.Cloud.VersionManagement.DataAccess.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        
+
         [Required]
         [DefaultValue(VersionComponent.Build)]
         public VersionComponent IncrementBy { get; set; } = VersionComponent.Build;
-        
+
         [Required]
         public int StartVersionMajor { get; set; }
-        
+
         [Required]
         public int StartVersionMinor { get; set; }
-        
+
         [Required]
         public int StartVersionBuild { get; set; }
-        
+
         [Required]
         public int StartVersionRevision { get; set; }
-        
+
         [Required]
         public int EndVersionMajor { get; set; }
-        
+
         [Required]
         public int EndVersionMinor { get; set; }
-        
+
         [Required]
         public int EndVersionBuild { get; set; }
-        
+
         [Required]
         public int EndVersionRevision { get; set; }
 
