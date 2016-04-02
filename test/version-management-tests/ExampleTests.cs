a938f4f7aaea21cb083b513ab4d09b2dd94f07b2
tintoy <tintoy@tintoy.io>
2016-04-03 09:43:04 +1000

    Add test project (currently includes example unit and integration tests).

---
+++ test/version-management-tests/ExampleTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.TestHost;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DD.Cloud.VersionManagement.Tests
+{
+	/// <summary>
+	///		Some example unit tests.
+	/// </summary>
+	public class ExampleTests
+    {
+		/// <summary>
+		///		A test that passes.
+		/// </summary>
+		[Fact]
+		public void This_Test_Passes()
+		{
+			Assert.Equal(true, true);
+			Assert.Equal(false, false);
+			Assert.NotEqual(true, false);
+			Assert.NotEqual(false, true);
+		}
+
+		/// <summary>
+		///		Verify that the version management application can be started.
+		/// </summary>
+		/// <returns>
+		///		A <see cref="Task"/> representing asynchronous test execution.
+		/// </returns>
+		[Fact]
+		public async Task Can_Start_Application()
+		{
+			WebHostBuilder appBuilder =
+				TestServer.CreateBuilder()
+					.UseStartup<Startup>();
+
+			using (TestServer testServer = new TestServer(appBuilder))
+			using (HttpClient testClient = testServer.CreateClient())
+			{
+				using (HttpResponseMessage response = await testClient.GetAsync("/"))
+				{
+					response.EnsureSuccessStatusCode();
+				}
+			}
+		}
+    }
+}
