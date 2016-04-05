6ce876e8d9ac5eabdaf692e6b9dafdaae209721a
tintoy <tintoy@tintoy.io>
2016-04-05 10:12:23 +1000

    Override the application environment to use correct base directory for tests involving Razor views.

--- test/VersionManagement.FunctionalTests/ExampleFunctionalTests.cs
+++ test/VersionManagement.FunctionalTests/ExampleFunctionalTests.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace DD.Cloud.VersionManagement.FunctionalTests
 {
+	using Utilities;
+
 	/// <summary>
 	///		Some example functional tests.
 	/// </summary>
@@ -14,7 +19,23 @@ namespace DD.Cloud.VersionManagement.FunctionalTests
         /// <summary>
         ///     The application pipeline builder for end-to-end tests.
         /// </summary>
-        readonly WebHostBuilder TestAppBuilder = TestServer.CreateBuilder().UseStartup<Startup>(); // AF: Can we then override service registrations to mock out things like data access?
+        readonly WebHostBuilder TestAppBuilder =
+			TestServer
+				.CreateBuilder()
+				.UseServices(services =>
+				{
+					// Slightly ugly hack.
+					IApplicationEnvironment originalEnvironment = CallContextServiceLocator.Locator.ServiceProvider.GetRequiredService<IApplicationEnvironment>();
+
+					// Shim the application environment so we point to the correct base path.
+					services.AddInstance(typeof(IApplicationEnvironment),
+						new TestApplicationEnvironment(originalEnvironment,
+							applicationName: "VersionManagement",
+							basePath: Path.GetFullPath(@"..\..\src\VersionManagement")
+						)
+					);
+				})
+				.UseStartup<Startup>();
         
 		/// <summary>
 		///		Verify that the home page can be retrieved.
@@ -38,5 +59,28 @@ namespace DD.Cloud.VersionManagement.FunctionalTests
 				}
 			}
 		}
-    }
+
+		/// <summary>
+		///		Verify that the product listing page can be retrieved.
+		/// </summary>
+		/// <returns>
+		///		A <see cref="Task"/> representing asynchronous test execution.
+		/// </returns>
+		[Fact]
+		public async Task Can_Get_Products_Page()
+		{
+			using (TestServer testServer = new TestServer(TestAppBuilder))
+			using (HttpClient testClient = testServer.CreateClient())
+			{
+				using (HttpResponseMessage response = await testClient.GetAsync("/products"))
+				{
+					response.EnsureSuccessStatusCode();
+
+					Assert.Equal("text/html",
+						response.Content.Headers.ContentType.MediaType
+					);
+				}
+			}
+		}
+	}
 }
---
+++ test/VersionManagement.FunctionalTests/Utilities/TestApplicationEnvironment.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.Runtime.Versioning;
+
+namespace DD.Cloud.VersionManagement.FunctionalTests.Utilities
+{
+	/// <summary>
+	///		An <see cref="IApplicationEnvironment"/> shim for use in tests.
+	/// </summary>
+	/// <remarks>
+	///		Used to override the application name and base path.
+	/// </remarks>
+	public class TestApplicationEnvironment
+		: IApplicationEnvironment
+	{
+		/// <summary>
+		///		Create a new <see cref="TestApplicationEnvironment"/>.
+		/// </summary>
+		/// <param name="originalEnvironment">
+		///		The original <see cref="IApplicationEnvironment"/>.
+		/// </param>
+		/// <param name="applicationName">
+		///		The application name.
+		/// </param>
+		/// <param name="basePath">
+		///		The application base path.
+		/// </param>
+		public TestApplicationEnvironment(IApplicationEnvironment originalEnvironment, string applicationName, string basePath)
+		{
+			if (originalEnvironment == null)
+				throw new ArgumentNullException(nameof(originalEnvironment));
+
+			if (String.IsNullOrWhiteSpace(applicationName))
+				throw new ArgumentException("Argument cannot be null, empty, or composed entirely of whitespace: 'applicationName'.", nameof(applicationName));
+
+			if (String.IsNullOrWhiteSpace(basePath))
+				throw new ArgumentException("Argument cannot be null, empty, or composed entirely of whitespace: 'basePath'.", nameof(basePath));
+
+			OriginalEnvironment = originalEnvironment;
+			ApplicationName = applicationName;
+			ApplicationBasePath = basePath;
+		}
+
+		IApplicationEnvironment OriginalEnvironment { get; }
+
+		/// <summary>
+		///		The current application name.
+		/// </summary>
+		public string ApplicationName { get; }
+
+		/// <summary>
+		///		The application version.
+		/// </summary>
+		public string ApplicationVersion => OriginalEnvironment.ApplicationVersion;
+
+		/// <summary>
+		///		The application base path.
+		/// </summary>
+		public string ApplicationBasePath { get; }
+
+		/// <summary>
+		///		The runtime framework used by the application.
+		/// </summary>
+		public FrameworkName RuntimeFramework => OriginalEnvironment.RuntimeFramework;
+
+		/// <summary>
+		///     Get the specified value from Application Global Data.
+		/// </summary>
+		/// <param name="name">
+		///     The name of the Application Global Data item to retrieve.
+		/// </param>
+		/// <returns>
+		///     The value of the item identified by <paramref name="name" />, or null if no item exists.
+		/// </returns>
+		public object GetData(string name) => OriginalEnvironment.GetData(name);
+
+		/// <summary>
+		///     Set the specified value in Application Global Data.
+		/// </summary>
+		/// <param name="name">
+		///     The name of the Application Global Data item to set.
+		/// </param>
+		/// <param name="value">
+		///     The value to store in Application Global Data.
+		/// </param>
+		public void SetData(string name, object value) => OriginalEnvironment.SetData(name, value);
+
+		/// <summary>
+		///     Get the application configuration.
+		/// </summary>
+		/// <remarks>
+		///		This should only be used for runtime compilation.
+		/// </remarks>
+		public string Configuration => OriginalEnvironment.Configuration;
+	}
+}
