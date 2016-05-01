051641a6cf6899bef203f7e74fc8f43d45e8acce
tintoy <tintoy@tintoy.io>
2016-05-02 09:58:42 +1000

    *nix test script should stop on the first test project that fails.

--- run-tests.sh
+++ run-tests.sh
@@ -4,12 +4,26 @@ testProjects=`ls -d1 ./test/*.Tests`
 for testProject in $testProjects; do
 	echo "Running unit tests for project \"$testProject\"."
 	dnx -p "$testProject" test -verbose
+	EXITCODE=$?
+
+	if [ $EXITCODE != 0 ]; then
+		echo "dnx test returned exit code ${EXITCODE}."
+
+		exit 1
+	fi
 done
 
 functionalTestProjects=`ls -d1 ./test/*.FunctionalTests`
 for testProject in $testProjects; do
 	echo "Running functional tests for project \"$testProject\"."
 	dnx --appbase "./src/VersionManagement" -p "$testProject" test -verbose
+	EXITCODE=$?
+	
+	if [ $EXITCODE != 0 ]; then
+		echo "dnx test returned exit code ${EXITCODE}."
+		 
+		exit 1
+	fi
 done
 
 echo "Done."
